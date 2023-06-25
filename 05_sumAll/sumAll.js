53ba95e7702a8b8b44299ca8716feb4dbbba34c9
sungseanpark <sungseanpark@gmail.com>
2023-06-24 20:46:28 -0700

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != "number" || typeof(num2) != "number") {
+        return 'ERROR';
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    let bigNum;
+    let smallNum;
+    if (num1 > num2) {
+        bigNum = num1;
+        smallNum = num2;
+    }
+    else{
+        bigNum = num2;
+        smallNum = num1;
+    }
+    
+    let result = 0;
+
+    for (let i = smallNum; i <= bigNum; i++){
+        result += i;
+    }
+
+    return result;
 
 };
 

79d4b3dbe2b1816bd5eeb71067894a76f7d143f8
sungseanpark <sungseanpark@gmail.com>
2023-06-24 20:06:17 -0700

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    if (stringToReverse.length == 0){
+        return '';
+    }
+
+    let result = '';
+    const splitString = stringToReverse.split('');
+    for(let i = stringToReverse.length -1; i >=0; i--){
+        result += splitString[i];
+
+    }
+    
+    return result;
 
 };
 

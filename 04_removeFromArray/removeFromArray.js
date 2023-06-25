c5cca3d0c2b0b6f5642eb3659db3f2b70048ab82
sungseanpark <sungseanpark@gmail.com>
2023-06-24 20:38:52 -0700

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
 const removeFromArray = function() {
+    let originalArray = arguments[0];
+    let result = [];
+    for (let i = 0; i < originalArray.length; i ++) {
+        let match = false;
+        for (let j = 1; j < arguments.length; j ++) {
+            if (originalArray[i] === arguments[j]) {
+                match = true;
+            }
+        }
+
+        if(!match) {
+            result.push(originalArray[i]);
+        }
+    }
+
+    return result;
 
 };
 

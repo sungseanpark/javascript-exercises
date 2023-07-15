008348e30f73a9567d4210a6b17f6b7891359fdf
sungseanpark <sungseanpark@gmail.com>
2023-07-15 16:23:21 -0700

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
+  return arguments[0] - arguments[1];
 	
 };
 
 const sum = function() {
+  return arguments[0].reduce((sum, current) => sum + current, 0);
 	
 };
 
 const multiply = function() {
+  arr = Array.from(arguments);
+  return arr.reduce((total, current) => total * current, 1);
 
 };
 
-const power = function() {
+const power = function(base, power) {
+  return Math.pow(base, power);
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let result = 1;
+  for(let i = 1; i <= num; i ++){
+    result = result * i;
+  }
+  return result;
 	
 };
 

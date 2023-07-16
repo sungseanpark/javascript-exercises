4e4a1f8ea63ed0974f89fc1d8970e30c4aa792c6
sungseanpark <sungseanpark@gmail.com>
2023-07-15 17:33:43 -0700

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort( function(a,b) {
+        let lastPerson;
+        if(!a['yearOfDeath']){
+            lastPerson = 2023 - a.yearOfBirth;
+        }
+        else{
+            lastPerson = a.yearOfDeath - a.yearOfBirth;
+        }
+        let nextPerson;
+        if(!b['yearOfDeath']){
+            nextPerson = 2023 - b.yearOfBirth;
+        }
+        else{
+            nextPerson = b.yearOfDeath - b.yearOfBirth;
+        }
+      return lastPerson > nextPerson ? -1 : 1;
+    });
+
+
+    return oldest[0];
 
 };
 
